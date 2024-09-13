be9edf9cfd7418a18e46441781f58abddc9d8fc6
周睿 <zrufo747@outlook.com>
2024-09-13 16:38:47 +0800

    uboot load

--- xtask/src/compile.rs
+++ xtask/src/compile.rs
@@ -62,30 +62,7 @@ impl Compile {
         println!("kernel image size: {:#}", Byte::from_u64(img_size));
 
         let bin_data = std::fs::read(&bin).unwrap();
-        let _ = fs::remove_file(&project.kernel_linux_image);
 
-        let mut image_file = fs::File::create(&project.kernel_linux_image).unwrap();
-
-        let mut header = ImageHeader {
-            magic: MAGIC,
-            image_size: img_size,
-            text_offset: 0x80000,
-            ..Default::default()
-        };
-
-        let header_data = unsafe {
-            std::slice::from_raw_parts_mut(
-                &mut header as *mut _ as *mut u8,
-                size_of::<ImageHeader>(),
-            )
-        };
-
-        header_data[0..8].copy_from_slice(&bin_data[0..8]);
-
-        image_file.write_all(header_data).unwrap();
-        image_file.write_all(&bin_data).unwrap();
-
-        println!("Image: {}", project.kernel_linux_image.display());
 
         Self {
             kernel_bin_path: image_elf_path,
@@ -93,20 +70,6 @@ impl Compile {
     }
 }
 
-const MAGIC: u32 = 0x644d5241;
 
-/* See Documentation/arm64/booting.txt in the Linux kernel */
-#[repr(C)]
-#[derive(Default)]
-struct ImageHeader {
-    code0: u32,       /* Executable code */
-    code1: u32,       /* Executable code */
-    text_offset: u64, /* Image load offset, LE */
-    image_size: u64,  /* Effective Image size, LE */
-    flags: u64,       /* Kernel flags, LE */
-    res2: u64,        /* reserved */
-    res3: u64,        /* reserved */
-    res4: u64,        /* reserved */
-    magic: u32,       /* Magic number */
-    res5: u32,
-}
+
+
