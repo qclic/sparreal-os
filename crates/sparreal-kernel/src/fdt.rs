d20114b1c87d38373a9c7d3c474045df44dcda99
周睿 <zrufo747@outlook.com>
2024-11-18 16:20:37 +0800

    fix

--- crates/sparreal-kernel/src/fdt.rs
+++ crates/sparreal-kernel/src/fdt.rs
@@ -2,6 +2,6 @@ pub use fdt_parser::*;
 
 use crate::driver::device_tree::get_device_tree;
 
-pub fn device_tree() -> Option<Fdt> {
+pub fn device_tree<'a>() -> Option<Fdt<'a>> {
     get_device_tree()
 }
