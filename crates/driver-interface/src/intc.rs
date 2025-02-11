0a16e6ef5a0c6f71502db29347a748fb160c2080
周睿 <zrufo747@outlook.com>
2025-02-11 15:12:35 +0800

    driver-interface

--- crates/driver-interface/src/intc.rs
+++ crates/driver-interface/src/intc.rs
@@ -10,19 +10,19 @@ pub type Hardware = Box<dyn Interface>;
 pub type OnProbeFdt = fn(crate::fdt::Node<'_>) -> Result<Hardware, Box<dyn Error>>;
 pub type HardwareCPU = Box<dyn InterfaceCPU>;
 
-pub trait InterfaceCPU: Send {
+pub trait InterfaceCPU: Send + Sync {
     fn get_and_acknowledge_interrupt(&mut self) -> Option<IrqId>;
     fn end_interrupt(&mut self, irq: IrqId);
-    fn irq_enable(&mut self, irq: IrqId);
-    fn irq_disable(&mut self, irq: IrqId);
-    fn set_priority(&mut self, irq: IrqId, priority: usize);
-    fn set_trigger(&mut self, irq: IrqId, triger: Trigger);
-    fn set_bind_cpu(&mut self, irq: IrqId, cpu_list: &[CpuId]);
     fn parse_fdt_config(&self, prop_interrupts: &[u32]) -> Result<IrqConfig, Box<dyn Error>>;
 }
 
 pub trait Interface: DriverGeneric {
     fn current_cpu_setup(&self) -> HardwareCPU;
+    fn irq_enable(&mut self, irq: IrqId);
+    fn irq_disable(&mut self, irq: IrqId);
+    fn set_priority(&mut self, irq: IrqId, priority: usize);
+    fn set_trigger(&mut self, irq: IrqId, triger: Trigger);
+    fn set_bind_cpu(&mut self, irq: IrqId, cpu_list: &[CpuId]);
 }
 
 /// The trigger configuration for an interrupt.
